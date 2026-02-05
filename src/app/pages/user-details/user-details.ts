65e4e1318a674c7ca0c6f45cd221d9083f0db835
Youssef Abboud <yousboud@hotmail.com>
2026-02-05 15:24:36 +0200

    user-details and user done UI

--- src/app/pages/user-details/user-details.ts
+++ src/app/pages/user-details/user-details.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AsyncPipe, NgIf, Location } from '@angular/common';
+import { ApiService } from '../../core/api';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user-details',
-  imports: [],
+  standalone: true,
+  imports: [NgIf, AsyncPipe, RouterLink],
   templateUrl: './user-details.html',
   styleUrl: './user-details.scss',
 })
 export class UserDetails {
+
+  private api = inject(ApiService);
+  private route = inject(ActivatedRoute);
+  private location = inject(Location);
+
+  id = Number(this.route.snapshot.paramMap.get('id'));
+
+  user$ = this.api.getUserById(this.id);
+
+  goBack(){
+    this.location.back();
+  }
 
 }
